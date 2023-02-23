096a104f0a943b460d0d4e43504916b79730217b
drizzyposh <femzyfadayomi@gmail.com>
2023-02-23 04:09:49 -0800

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File 	-	main.h
+ * Author	-	Oluwafemi fadayomi
+ * Desc: Header contains protypes for all functions
+ * used in 0x04-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+#endif
