98b05077eddcb8f276d533d8ad0173ef74f806eb
drizzyposh <femzyfadayomi@gmail.com>
2023-03-22 05:20:15 -0700

    2-int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,8 +16,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (k = 0; k < size; k++)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (cmp(array[k]))
+			return (k);
 	}
 
 	return (-1);
